1941536beb5f7eb5938ead02bbc246b27ec0556c
night <40443125@gm.nfu.edu.tw>
2019-12-18 22:14:26 +0800

    Google sign_in(FirebaseAuth)

--- app/src/main/java/com/night/bshop/SigninActivity.kt
+++ app/src/main/java/com/night/bshop/SigninActivity.kt
@@ -1,19 +1,40 @@
 package com.night.bshop
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_signin.*
+import java.lang.Exception
 
 class SigninActivity : AppCompatActivity() {
+
+    private val RC_GOOGLE_SIGN_IN: Int = 200
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val TAG = SigninActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
 
-        val auth = FirebaseAuth.getInstance()
+        //google sign in object
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        google_sign_in.setOnClickListener{
+            startActivityForResult(googleSignInClient.signInIntent,RC_GOOGLE_SIGN_IN)
+        }
 
         button_sigUp.setOnClickListener { view ->
             signUp()
@@ -22,6 +43,28 @@ class SigninActivity : AppCompatActivity() {
         button_SignIn.setOnClickListener { task ->
             signIn()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        // getResult 有時候會失敗給予ApiException class
+        val account = task.getResult(ApiException::class.java)
+        Log.d(TAG,"onActivityResult ${account?.id}")
+        //物件傳到Firebase取得帳號並建立資料
+        val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        FirebaseAuth.getInstance()
+            .signInWithCredential(credential)
+            .addOnCompleteListener{ task ->
+                if(task.isSuccessful) {
+                    setResult(Activity.RESULT_OK)
+                    finish()
+                } else {
+                    Log.d(TAG,"onActivityResult ${task.exception?.message}")
+                    Snackbar.make(main_sign_in,"Firebase authentication failed",Snackbar.LENGTH_LONG)
+                }
+            }
+
     }
 
     private  fun signUp(){
